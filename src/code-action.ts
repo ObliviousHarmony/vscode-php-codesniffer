47e2b07f0362165266001ca82385b2f429a26092
Christopher Allford <christopher.allford@automattic.com>
2021-02-12 11:01:03 -0800

    Added Custom CodeAction Class
    This class will allow us to add properties to our code actions.
    This is useful as otherwise there is no easy way to associate
    the action with the document that they belong to when resolving.

---
+++ src/code-action.ts
@@ -0,0 +1,16 @@
+import { CodeAction as BaseCodeAction, TextDocument, Uri } from 'vscode';
+
+/**
+ * A custom code action class that adds a Uri for associating it with a document.
+ */
+export class CodeAction extends BaseCodeAction {
+    /**
+     * The document associated with the code action.
+     */
+    public document?: TextDocument;
+}
+
+/**
+ * A collection of CodeAction instances keyed by the Uri.
+ */
+export class CodeActionCollection extends Map<Uri, CodeAction[]> {}
